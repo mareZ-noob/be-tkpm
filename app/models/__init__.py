c73d19d3a080f6c6acd4db48e9f4d1c6478222d5
Nguyen Thanh Nam <namnguyen160504@gmail.com>
2025-05-09 02:15:34 +0700

    fix: remove table

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -3,6 +3,5 @@ from .document import Document
 from .image import Image
 from .reset_password_token import ResetPasswordToken
 from .user import User
-from .verification_token import VerificationToken
 from .video import Video
 from .youtube_upload import YoutubeUpload
